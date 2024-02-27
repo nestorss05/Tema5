021a7fa1399d3b1b200fc91c085e1054c1e76baf
nestorss05 <nestor.sanchez@iesnervion.es>
2024-02-27 13:42:46 +0100

    Ejercicio 1 FINAL

--- src/ejercicio01/CuentaCorriente.java
+++ src/ejercicio01/CuentaCorriente.java
@@ -44,29 +44,61 @@ public class CuentaCorriente {
 	}
 
 	/**
-	 * Constructor con casi todos los parametros
-	 * 
-	 * @param dni:   dni del usuario
-	 * @param saldo: saldo inicial del usuario
-	 */
-	public CuentaCorriente(String dni, int saldo) {
-		super();
-		this.dni = dni;
-		this.saldo = saldo;
-	}
-
-	/**
 	 * Funcion sacarDinero: saca dinero de la cuenta
 	 * 
-	 * @param dni    dni del usuario
-	 * @param saldo: saldo inicial del usuario
+	 * @param cantidad de dinero a retirar
 	 * @return: si se ha podido hacer la operacion o no
 	 */
-	public boolean sacarDinero(String dni, int saldo) {
+	public boolean sacarDinero(int cantidad) {
 
 		// boolean esExitoso: booleana que indica si la funcion ha salido bien o no (por
 		// defecto, es falso)
 		boolean esExitoso = false;
+
+		// If: si la cantidad es menor que el saldo, se le restara al saldo la cantidad,
+		// y esExitoso sera true
+		if (cantidad < saldo) {
+			saldo = saldo - cantidad;
+			esExitoso = true;
+		} // Fin If
+
+		// Devuelve esExitoso al main
+		return esExitoso;
+
+	}
+
+	/**
+	 * Funcion ingresarDinero: ingresa dinero a la cuenta
+	 * 
+	 * @param cantidad de dinero a ingresar
+	 * @return: si se ha podido hacer la operacion o no
+	 */
+	public boolean ingresarDinero(int cantidad) {
+
+		// boolean esExitoso: booleana que indica si la funcion ha salido bien o no (por
+		// defecto, es falso)
+		boolean esExitoso = false;
+
+		// If: si la cantidad es mayor que 0, se le sumara al saldo la cantidad, y
+		// esExitoso sera true
+		if (cantidad > 0) {
+			saldo = saldo + cantidad;
+			esExitoso = true;
+		} // Fin If
+
+		// Devuelve esExitoso al main
+		return esExitoso;
+
+	}
+
+	/**
+	 * Funcion mostrarInformacion: muestra informacion de la cuenta
+	 */
+	public void mostrarInformacion() {
+
+		System.out.println("DNI: " + dni);
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Saldo: " + saldo + " NS$");
 
 	}
 
--- src/ejercicio01/MainCode.java
+++ src/ejercicio01/MainCode.java
@@ -26,50 +26,89 @@ public class MainCode {
 		// String nombre: nombre del usuario
 		String nombre = "";
 
-		// String saldo: saldo del usuario
+		// int saldo: saldo del usuario
 		int saldo = 0;
+		
+		// int cantidad: cantidad de dinero a ingresar o sacar
+		int cantidad = 0;
 
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
 
-		// Haz referencia a CuentaCorriente
-		CuentaCorriente cc = new CuentaCorriente(dni, saldo);
+		// Pide al usuario el DNI
+		System.out.println("Inserta el DNI");
+		dni = sc.nextLine();
 
-		// Haz referencia a CuentaCorriente pero con nombre incluido
-		CuentaCorriente ccFull = new CuentaCorriente(dni, nombre, saldo);
+		// Pide al usuario tu nombre
+		System.out.println("Inserta tu nombre");
+		nombre = sc.nextLine();
+
+		// Haz referencia a CuentaCorriente
+		CuentaCorriente cc = new CuentaCorriente(dni, nombre, saldo);
 
 		// Do-While: pide al usuario una opcion a elegir
 		do {
+			// Try-catch: si la opcion no es un numero, el programa te mostrara que la
+			// respuesta es invalida
 			try {
 				System.out.println(
 						"BANCO CENTRAL DE LA REPUBLICA NESTORIANA \n 1. Sacar dinero \n 2. Ingresar dinero \n 3. Mostrar informacion \n 0. Salir");
 				opcion = sc.nextInt();
+
+				// Switch: lo que el programa procedera a hacer segun la opcion seleccionada
 				switch (opcion) {
 
+				// Case 0: se saldra del programa
 				case 0 -> {
-					System.out.println("Saliendo del programa...");
+					System.out.println("Sesion cerrada con exito");
 				}
+
+				// Case 1: se sacara dinero de la cuenta
 				case 1 -> {
-					System.out.println("Inserta el DNI");
-					dni = sc.nextLine();
-					System.out.println(cc.sacarDinero(dni, saldo));
+					try {
+						System.out.println("Inserta la cantidad de dinero a retirar");
+						cantidad = sc.nextInt();
+						System.out.println(cc.sacarDinero(cantidad));
+					} catch (InputMismatchException e) {
+						System.out.println("ERROR: Cantidad introducida invalida. La operacion ha sido anulada.");
+						cantidad = 0;
+					} finally {
+						System.out.println("Presione una tecla para continuar");
+						sc.nextLine();
+					}
 				}
+
+				// Case 2: se ingresara dinero a la cuenta
 				case 2 -> {
-					System.out.println("");
+					try {
+						System.out.println("Inserta la cantidad de dinero a ingresar");
+						cantidad = sc.nextInt();
+						System.out.println(cc.ingresarDinero(cantidad));
+					} catch (InputMismatchException e) {
+						System.out.println("ERROR: Cantidad introducida invalida. La operacion ha sido anulada.");
+						cantidad = 0;
+					} finally {
+						System.out.println("Presione una tecla para continuar");
+						sc.nextLine();
+					}
 				}
+
+				// Case 3: se mostrara informacion de la cuenta
 				case 3 -> {
-					System.out.println("");
+					cc.mostrarInformacion();
 				}
+
+				// Default: opcion invalida
 				default -> {
 					System.out.println("ERROR: Opcion invalida");
 				}
 
-				}
+				} // Fin Switch
 			} catch (InputMismatchException e) {
 				System.out.println("ERROR: Respuesta invalida");
 			} finally {
 				sc.nextLine();
-			}
+			} // Fin Try--Catch
 
 		} while (opcion != 0);
 
