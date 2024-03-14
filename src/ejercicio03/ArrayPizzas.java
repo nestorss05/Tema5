f5da1036527dbf5b707393fc4b6de39881f424da
nestorss05 <nestor.sanchez@iesnervion.es>
2024-03-14 20:12:18 +0100

    Ejercicio 3 FINAL

--- src/ejercicio03/ArrayPizzas.java
+++ src/ejercicio03/ArrayPizzas.java
@@ -7,6 +7,136 @@ package ejercicio03;
  */
 public class ArrayPizzas {
 
-	
-	
+	/**
+	 * Pizza[] pizzas: lista de pizzas
+	 */
+	public static Pizza[] pizzas = new Pizza[50];
+
+	/**
+	 * Funcion mostrarPizzas: muestra las pizzas
+	 * 
+	 * @param pedido
+	 */
+	public static void mostrarPizzas(Pizza pedido) {
+
+		// For-Each: analiza todo el array de pizzas y muestra los espacios no vacios en
+		// pantalla
+		for (Pizza pedidoN : pizzas) {
+			// If-Else: si el pedido no es nulo, se mostrara por pantalla. De lo contrario,
+			// se hara un break
+			if (pedidoN != null) {
+				System.out.println(pedidoN);
+			} else {
+				break;
+			} // Fin If-Else
+		} // Fin For-Each
+
+	}
+
+	/**
+	 * Funcion nuevoPedido: añade un pedido al array
+	 * 
+	 * @param pedido pedido a añadir
+	 * @return si se ha podido añadir el pedido o no
+	 */
+	public static boolean nuevoPedido(Pizza pedido) {
+
+		// boolean exitoso: muestra si ha salido bien la operacion o no
+		boolean exitoso = false;
+
+		// int pos: posicion del array a buscar
+		int pos = 0;
+
+		// int i: posicion del array a buscar espacios null
+		int i = 0;
+
+		// Busca la posicion
+		pos = buscaPosicion(pedido);
+
+		// If: si la posicion es -1, se procedera a añadir la pizza
+		if (pos == -1) {
+
+			// While: avanza i mientras i sea menor que pizzas.length y pizzas[i] no sea
+			// nulo
+			while (i < pizzas.length && pizzas[i] != null) {
+				i++;
+			} // Fin While
+
+			// If: si i es menor que pizzas.length, se añadira la pizza
+			if (i < pizzas.length) {
+
+				// Añade la pizza al pedido
+				pizzas[i] = pedido;
+
+				// Pon el estado de la pizza como pedida
+				pizzas[i].setEstado("Pedida");
+
+				// exitoso sera true
+				exitoso = true;
+
+			} // Fin If
+
+		} // Fin If
+
+		// Devuelve exitoso al main
+		return exitoso;
+	}
+
+	/**
+	 * Funcion pizzaServida: pon la pizza como servida
+	 * 
+	 * @param pedido pedido a modificar
+	 * @return si se ha podido servir la pizza o no
+	 */
+	public static boolean pizzaServida(Pizza pedido) {
+
+		// boolean exitoso: muestra si ha salido bien la operacion o no
+		boolean exitoso = false;
+
+		// int pos: posicion del array del pedido a modificar
+		int pos = 0;
+
+		// Busca la posicion
+		pos = buscaPosicion(pedido);
+
+		// If: si la posicion es mayor o igual a 0, se procedera a modificar el pedido
+		if (pos >= 0) {
+
+			// Pon el estado de la pizza como pedida
+			pizzas[pos].setEstado("Servida");
+
+			// exitoso sera true
+			exitoso = true;
+
+		} // Fin If
+
+		// Devuelve exitoso al main
+		return exitoso;
+
+	}
+
+	/**
+	 * Funcion buscaPosicion: busca la posicion del pedido
+	 * 
+	 * @param pedido pedido a buscar
+	 * @return posicion del pedido
+	 */
+	private static int buscaPosicion(Pizza pedido) {
+		int pos = -1;
+		int i = 0;
+
+		while (i < pizzas.length && pos == -1) {
+			// Compruebo que la posición no sea null
+			// Compruebo si el artículo de la posición i es igual a art (artículo pasado por
+			// parámetro)
+			if (pizzas[i] != null && pizzas[i].equals(pedido)) {
+				pos = i;
+			}
+
+			i++;
+		}
+
+		return pos;
+	}
+
 }
--- src/ejercicio03/Principal.java
+++ src/ejercicio03/Principal.java
@@ -10,6 +10,24 @@ public class Principal {
 		// int opcion: opcion elegida por el usuario
 		int opcion = 0;
 
+		// int codigo: codigo de la pizza
+		int codigo = -1;
+
+		// String tamaño: tamaño de la pizza
+		String tamaño;
+
+		// String tipo: tipo de la pizza
+		String tipo;
+
+		// Pizza pedido: pedido de la pizza
+		Pizza pedido = null;
+
+		// boolean accionExitosa: muestra si una accion salio bien o no
+		boolean accionExitosa = false;
+
+		// boolean numValido: comprueba si un numero es valido o no
+		boolean numValido = false;
+
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
 
@@ -32,28 +50,104 @@ public class Principal {
 
 			// Case 1: listado de pizzas
 			case 1 -> {
-				
-			}
+
+				// Muestra las pizzas por pantalla
+				ArrayPizzas.mostrarPizzas(pedido);
+
+			} // Fin Case 1
 
 			// Case 2: nuevo pedido
 			case 2 -> {
-				
-			}
+
+				// Do-While: Pide al usuario el codigo
+				System.out.println("Inserta el codigo");
+				do {
+					try {
+						numValido = false;
+						codigo = sc.nextInt();
+						if (codigo < 0) {
+							System.out.println("ERROR: el codigo no puede ser negativo");
+						} else {
+							numValido = true;
+						}
+					} catch (InputMismatchException e) {
+						System.out.println("ERROR: respuesta invalido");
+					} finally {
+						sc.nextLine();
+					}
+				} while (!numValido); // Fin Do-While
+
+				// Pide al usuario el tamaño de la pizza
+				System.out.println("¿De que tamaño quieres que sea la pizza? ¿Mediana o Familiar?");
+				tamaño = sc.next();
+				sc.nextLine();
+
+				// Pide al usuario el tipo de la pizza
+				System.out.println("¿De que quieres la pizza? ¿Margarita, CuatroQuesos, o Funghi?");
+				tipo = sc.next();
+				sc.nextLine();
+
+				// Añade el pedido
+				pedido = new Pizza(codigo, tamaño, tipo);
+
+				// Añade la pizza al array
+				accionExitosa = ArrayPizzas.nuevoPedido(pedido);
+
+				// If-Else: Si se ha podido añadir la pizza o no, se le mostrara por pantalla el
+				// resultado de la opperacion
+				if (accionExitosa) {
+					System.out.println("Se ha añadido la pizza correctamente");
+				} else {
+					System.out.println("ERROR: no se ha podido añadir la pizza");
+				} // Fin If-Else
+
+			} // Fin Case 2
 
 			// Case 3: pizza servida
 			case 3 -> {
-				
-			}
+				// Do-While: Pide al usuario el codigo
+				System.out.println("Inserta el codigo");
+				do {
+					try {
+						numValido = false;
+						codigo = sc.nextInt();
+						if (codigo < 0) {
+							System.out.println("ERROR: el codigo no puede ser negativo");
+						} else {
+							numValido = true;
+						}
+					} catch (InputMismatchException e) {
+						System.out.println("ERROR: respuesta invalido");
+					} finally {
+						sc.nextLine();
+					}
+				} while (!numValido); // Fin Do-While
+
+				// Añade el pedido modificado
+				pedido = new Pizza(codigo);
+
+				// Modifica la pizza para que este servida
+				accionExitosa = ArrayPizzas.pizzaServida(pedido);
+
+				// If-Else: Si se ha podido servir la pizza o no, se le informara del estado de
+				// la accion
+				if (accionExitosa) {
+					System.out.println("La pizza se ha servido");
+				} else {
+					System.out.println("ERROR: no se ha podido servir la pizza");
+				} // Fin If-Else
+
+			} // Fin Case 3
 
 			// Case 4: sal del programa
 			case 4 -> {
-				System.out.println("Saliendo del programa");
-			}
+				System.out.println("Saliendo del programa...");
+			} // Fin Case 4
 
 			// Default: opcion invalida
 			default -> {
 				System.out.println("ERROR: opcion invalida");
-			}
+			} // Fin Default
 
 			} // Fin Switch
 
