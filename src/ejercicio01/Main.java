de5488f5f0afe0db7b0de540a72633bc560be9b3
nestorss05 <nestor.sanchez@iesnervion.es>
2024-03-19 13:20:29 +0100

    Preparaciones para empezar el ejercicio

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -7,104 +7,93 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// nombre: nombre del alumno
-		String nombre = "";
+		// String titulo: titulo del libro
+		String titulo = "";
 
-		// double notaMedia: nota media del alumno
-		double notaMedia = 0;
+		// String autor: autor del libro
+		String autor = "";
 
-		// int opcion: opcion elegida por el usuario
-		int opcion = 0;
+		// int cantidad: cantidad a pedir prestado / devolver
+		int cantidad = 0;
+
+		// int respuesta: respuesta para las 3 opciones del usuario
+		int respuesta = 0;
+
+		// boolean correcto: booleana que comprueba si un entero es correcto o no
+		boolean correcto = false;
 
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
+		
+		// Pide al usuario un titulo
+		System.out.println("Inserta el titulo del libro");
+		titulo = sc.nextLine();
 
-		// Do-While: el programa se ejecutara mientras que la opcion no sea 5
+		// Pide al usuario el autor
+		System.out.println("Inserta el autor del libro");
+		autor = sc.nextLine();
+
+		// Enlaza la clase Libro mediante lib
+		Libro lib = new Libro(titulo, autor, 10, 0);
+
+		// Do While
 		do {
 
 			// Muestra el menu
-			Main.mostrarMenu();
+			System.out.println("1. Prestamo");
+			System.out.println("2. Devolucion");
+			System.out.println("3. Mostrar informacion");
+			System.out.println("4. Salir");
 
-			// Pide al usuario una opcion
-			try {
-				opcion = sc.nextInt();
-			} catch (InputMismatchException e) {
-				System.out.println("ERROR: respuesta invalida");
-			} finally {
-				sc.nextLine();
-			} // Fin Try-Catch
-
-			// Por cada opcion, haz cierta accion
-			switch (opcion) {
-
-			// Case 1: imprime a todos los alumnos registrados
-			case 1 -> {
-				ArrayAlumno.mostrarAlumnos();
-			}
+			// Pide al usuario la opcion
+			do {
+				try {
+					respuesta = sc.nextInt();
+					correcto = true;
+				} catch (InputMismatchException e) {
+					System.out.println("ERROR: respuesta invalida");
+				} finally {
+					sc.nextLine();
+				} // Fin Try-Catch
+			} while (!correcto);
 			
-			// Case 2: añade un nuevo alumno
-			case 2 -> {
+			// Correcto volvera a ser false
+			correcto = false;
+			
+			// Switch respuesta
+			switch (respuesta) {
+			
+			// Case 1 y 2: Prestamo / Devolucion
+			case 1, 2 -> {
 				
-				// Pide al usuario el nombre
-				System.out.println("Inserta el nombre del alumno a añadir");
-				nombre = sc.nextLine();
-				
-				// Pide al usuario la nota
-				System.out.println("Inserta la nota nueva");
+				// Pide al usuario la cantidad
+				System.out.println("Inserta la cantidad de libros a pedir prestado / devolver");
 				do {
 					try {
-						notaMedia = sc.nextDouble();
+						cantidad = sc.nextInt();
 					} catch (InputMismatchException e) {
 						System.out.println("ERROR: respuesta invalida");
 					} finally {
 						sc.nextLine();
-					}
-				} while (notaMedia < 0 || notaMedia > 10);
+					} // Fin Try-Catch
+				} while (!correcto);
 				
-				// Inserta el alumno
-				ArrayAlumno.insertarAlumno(nombre, notaMedia);
+				if (respuesta == 1) {
+					System.out.println(lib.prestamo(cantidad));
+				} else {
+					
+				}
 				
 			}
-			
-			// Case 3: modifica la nota
+
+			// Case 3: muestra informacion del libro
 			case 3 -> {
-				
-				// Pide al usuario el nombre
-				System.out.println("Inserta el nombre del alumno a eliminar");
-				nombre = sc.nextLine();
-				
-				// Pide al usuario la nota
-				System.out.println("Inserta la nota nueva");
-				do {
-					try {
-						notaMedia = sc.nextDouble();
-					} catch (InputMismatchException e) {
-						System.out.println("ERROR: respuesta invalida");
-					} finally {
-						sc.nextLine();
-					}
-				} while (notaMedia < 0 || notaMedia > 10);
-				
-				// Modifica la nota
-				ArrayAlumno.modificarNota(nombre, notaMedia);
-				
+				lib.mostrarInfo();
 			}
 			
-			// Case 4: elimina a un alumno
+			// Case 4: sal del programa
 			case 4 -> {
-				
-				// Pide al usuario el nombre
-				System.out.println("Inserta el nombre del alumno a eliminar");
-				nombre = sc.nextLine();
-				
-				// Elimina el alumno
-				ArrayAlumno.eliminarAlumno(nombre);
-				
-			}
-
-			// Case 5: sal del programa
-			case 5 -> {
-				System.out.println("Saliendo del programa...");
+				System.out.println("Saliendo del programa");
 			}
 
 			// Default: opcion invalida
@@ -112,28 +101,13 @@ public class Main {
 				System.out.println("ERROR: opcion invalida");
 			}
 
-			}
-		} while (opcion != 5);
+			} // Fin Switch
+
+		} while (respuesta != 4); // Fin Do-While
 		
 		// Cierra el Scanner
 		sc.close();
 
-	}
-	
-	/**
-	 * Funcion mostrarMenu: muestra el menu
-	 */
-	public static void mostrarMenu() {
-		
-		System.out.println("ALUMNOS/AS");
-		System.out.println("==========");
-		System.out.println("1. Listado");
-		System.out.println("2. Nuevo Alumno");
-		System.out.println("3. Modificar");
-		System.out.println("4. Borrar");
-		System.out.println("5. Salir");
-		System.out.println("¿Qué opción quiere ejecutar?");
-		
 	}
 
 }
